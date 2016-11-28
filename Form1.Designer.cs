69a7b644d69d6110285500d2af3250c3d11604d1
Thomas Pomme <Thomas.benhamou@hotmail.fr>
2016-11-28 08:16:04 +0100

    add posters

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonDir = new System.Windows.Forms.Button();
+            this.checkBoxHide = new System.Windows.Forms.CheckBox();
+            this.panelTitleHide = new System.Windows.Forms.Panel();
+            this.imgBox = new System.Windows.Forms.PictureBox();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonNext.Location = new System.Drawing.Point(838, 380);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 1;
+            this.buttonNext.Text = ">>>>>";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonBack.Location = new System.Drawing.Point(12, 380);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 2;
+            this.buttonBack.Text = "<<<<<";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonDir
+            // 
+            this.buttonDir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonDir.Location = new System.Drawing.Point(12, 351);
+            this.buttonDir.Name = "buttonDir";
+            this.buttonDir.Size = new System.Drawing.Size(75, 23);
+            this.buttonDir.TabIndex = 3;
+            this.buttonDir.Text = "Directory";
+            this.buttonDir.UseVisualStyleBackColor = true;
+            this.buttonDir.Click += new System.EventHandler(this.buttonDir_Click);
+            // 
+            // checkBoxHide
+            // 
+            this.checkBoxHide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxHide.AutoSize = true;
+            this.checkBoxHide.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBoxHide.Location = new System.Drawing.Point(838, 328);
+            this.checkBoxHide.Name = "checkBoxHide";
+            this.checkBoxHide.Size = new System.Drawing.Size(67, 17);
+            this.checkBoxHide.TabIndex = 4;
+            this.checkBoxHide.Text = "Hide title";
+            this.checkBoxHide.UseVisualStyleBackColor = true;
+            this.checkBoxHide.CheckedChanged += new System.EventHandler(this.checkBoxHide_CheckedChanged);
+            // 
+            // panelTitleHide
+            // 
+            this.panelTitleHide.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelTitleHide.Location = new System.Drawing.Point(12, 31);
+            this.panelTitleHide.Name = "panelTitleHide";
+            this.panelTitleHide.Size = new System.Drawing.Size(893, 69);
+            this.panelTitleHide.TabIndex = 5;
+            this.panelTitleHide.Visible = false;
+            // 
+            // imgBox
+            // 
+            this.imgBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imgBox.Image = global::WindowsFormsApplication1.Properties.Resources.white;
+            this.imgBox.Location = new System.Drawing.Point(12, 12);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(901, 391);
+            this.imgBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgBox.TabIndex = 0;
+            this.imgBox.TabStop = false;
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRandom.Location = new System.Drawing.Point(838, 351);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(75, 23);
+            this.buttonRandom.TabIndex = 6;
+            this.buttonRandom.Text = "Random";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(925, 415);
+            this.Controls.Add(this.buttonRandom);
+            this.Controls.Add(this.panelTitleHide);
+            this.Controls.Add(this.checkBoxHide);
+            this.Controls.Add(this.buttonDir);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.imgBox);
+            this.Name = "Form1";
+            this.Text = "Minimal Poster";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgBox;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonDir;
+        private System.Windows.Forms.CheckBox checkBoxHide;
+        private System.Windows.Forms.Panel panelTitleHide;
+        private System.Windows.Forms.Button buttonRandom;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if(mesImages.Count>0)
+            imgBox.Image = mesImages.ElementAt(index=(index+1)%(mesImages.Count-1));
+        }
+
+        List<Image> mesImages = new List<Image>();
+       int index = 0;
+        string filepath;
+        private void buttonDir_Click(object sender, EventArgs e)
+        {
+            mesImages = new List<Image>();
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+
+                    imgBox.Image = Properties.Resources._01_progress;
+                    filepath = folderDialog.SelectedPath;
+
+                    Thread t = new Thread(new ThreadStart(DoWork));
+                    t.Start();
+
+
+                 }
+            }
+        }
+        private void DoWork()
+        {
+
+            DirectoryInfo d = new DirectoryInfo(filepath);
+
+            foreach (var file in d.GetFiles("*.jpg"))
+            {
+                mesImages.Add(Image.FromFile(file.FullName));
+            }
+
+            imgBox.Image = mesImages.ElementAt(index);
+        }
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            if(mesImages.Count>0)
+            imgBox.Image = mesImages.ElementAt(index = (index - 1)>0?index-1:0);
+        }
+
+        private void checkBoxHide_CheckedChanged(object sender, EventArgs e)
+        {
+            panelTitleHide.Visible = checkBoxHide.Checked;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            int height = (int)(Height * 0.17);
+            int top = (int)(Height * 0.07);
+            panelTitleHide.Size = new Size( Width, height);
+            panelTitleHide.Location = new Point(0, top);
+        }
+
+        Random r = new Random();
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+
+            if (mesImages.Count > 0)
+                imgBox.Image = mesImages.ElementAt(r.Next(0,mesImages.Count-1));
+        }
+    }
+}
